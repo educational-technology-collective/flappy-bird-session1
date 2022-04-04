97b36f85cfac3dec8d3b8e6c40b088847e2eafba
April Wang <aprilww@m-xp3q3p52gg.local>
2022-04-04 01:07:07 -0400

    ## Part 3 - Animating the Poles
    Now we want to animate the poles so they move from left to right when we start the game. The game made it looks like the bird is flying forward, but in fact, it's the poles that are moving, our bird can only move up and down.
    In order to do this, we are going to add animations to our JavaScript file.
    
    1. The first thing we will do is add querySelector on the elements, and initialize some variables including the bird's speed.

---
+++ index.js
@@ -0,0 +1,6 @@
+// Save DOM objects to variables
+const poles = document.querySelectorAll(".pole");
+const gameArea = document.querySelector("#game-area");
+const restartBtn = document.querySelector("#restart-btn");
+
+const speed = 2;
