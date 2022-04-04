56c911d6fa89ed72689750d183c19c29419f7f99
April Wang <aprilww@m-xp3q3p52gg.local>
2022-04-04 01:09:54 -0400

    Then we will generate a random height using `Math.random()` and set the two pole's heights accordingly. Then we need to set the new pole's right attribute to 0 in order to place it on the right side of the game area.

--- index.js
+++ index.js
@@ -26,8 +26,14 @@ function updatePoles() {
     //  Check whether the poles went putside of game area.
     if (polesCurrentPos > containerWidth) {
         // Generate new poles
+        let newHeight = parseInt(Math.random() * 100);
+        // Change the poles' height
+        //The middle gap of the pole is 100px
+        poles[0].style.height = `${100 + newHeight}px`;
+        poles[1].style.height = `${100 - newHeight}px`;
+
         // Move poles back to the right-hand side of game area.
-        polesCurrentPos = 0; // This is based on the "right" property.  
+        polesCurrentPos = 0; // This is based on the "right" property.
     }
 
     poles.forEach((pole) => {
